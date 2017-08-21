41927ed71feeee499566316b9286fbe94ad1b345
strange <oros.alexander@gmail.com>
2017-08-21 19:12:18 +0200

    touchinput test

--- arduino/orobot/cerebellum/cerebellum.ino
+++ arduino/orobot/cerebellum/cerebellum.ino
@@ -8,6 +8,7 @@
 
 void setup() {
   Serial.begin(9600);
+  Serial1.begin(9600);
   
   pinMode(M1D2, OUTPUT);
   pinMode(M1IN1, OUTPUT);
@@ -20,14 +21,72 @@ void setup() {
 
   digitalWrite(M1IN1, HIGH);
   digitalWrite(M1IN2, LOW);
-  analogWrite(M1D2, 90);
+  analogWrite(M1D2, 0);
   
   digitalWrite(M2IN1, HIGH);
   digitalWrite(M2IN2, LOW);
-  analogWrite(M2D2, 150);
+  analogWrite(M2D2, 0);
   
 }
 
 void loop() {
+  receive_serial_cmd();
+}
 
+void receive_serial_cmd(void) {
+  static uint8_t cmd[18];         // command buffer
+  static uint8_t cmdcount = 0;    // position in the buffer of the received byte
+  uint8_t c;                      // received byte
+  while(Serial1.available()) {
+    c = Serial1.read();
+    if(c > ' ') cmd[cmdcount++] = c;
+    if((c == 8) && (cmdcount > 0)) cmdcount--;                // deals with backspaces, if a person on the other side types 
+    if((c == 0x0d) || (c == 0x0a) || (cmdcount > 16)) {       // end of command, gets interpreted now
+      cmd[cmdcount] = 0;    // clear the last byte in cmd buffer
+      if(cmdcount > 0) {    // prevent empty cmd buffer parsing
+       switch(cmd[0]) {
+        case 'l': 
+          if((cmdcount > 2) && (cmdcount < 7)) {
+            int temp = atoi((const char *)&cmd[1]);           
+            Serial.print("l:");
+            Serial.println(temp);  
+            if(temp > 0) {
+              if(temp > 255) temp = 255;
+              digitalWrite(M1IN1, HIGH);
+              digitalWrite(M1IN2, LOW);
+              analogWrite(M1D2, temp);                    
+            } else {
+              temp = -temp;
+              if(temp > 255) temp = 255;
+              digitalWrite(M1IN1, LOW);
+              digitalWrite(M1IN2, HIGH);
+              analogWrite(M1D2, temp); 
+            }
+          }                    
+          break; 
+          case 'r':
+           if((cmdcount > 2) && (cmdcount < 7)) {
+            int temp = atoi((const char *)&cmd[1]);           
+            Serial.print("r:");
+            Serial.println(temp);
+            
+            if(temp > 0) {
+              if(temp > 255) temp = 255;
+              digitalWrite(M2IN1, HIGH);
+              digitalWrite(M2IN2, LOW);
+              analogWrite(M2D2, temp);                    
+            } else {
+              temp = -temp;
+              if(temp > 255) temp = 255;
+              digitalWrite(M2IN1, LOW);
+              digitalWrite(M2IN2, HIGH);
+              analogWrite(M2D2, temp); 
+            }                        
+          }  
+          break;                 
+       }    
+      }
+      cmdcount = 0;
+    } 
+  }
 }
---
+++ web/touchinput/script.js
@@ -0,0 +1,81 @@
+Number.prototype.map = function (in_min, in_max, out_min, out_max) {
+  return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+function init() {
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+	var rect = canvas.getBoundingClientRect();
+	var mdown = false;
+	var pos;
+	canvas.addEventListener("mousedown", inputStart, false);
+	canvas.addEventListener("mouseup", inputEnd, false);
+	canvas.addEventListener("mouseout", function(e) {
+		mdown = false;
+	}, false);
+	canvas.addEventListener('mousemove', inputMove, false);
+	canvas.addEventListener("touchstart", inputStart, false);
+	canvas.addEventListener("touchend", inputEnd, false);
+	canvas.addEventListener("touchmove", inputMove, false);
+
+	function inputStart() {
+		mdown = true;
+	}
+	function inputEnd() {
+	inputMove({ clientX: Math.round(rect.width/2+rect.left), clientY: Math.round(rect.height/2+rect.top)});
+		mdown = false;
+	}
+	function inputMove(e) {
+	document.getElementById("console").innerHTML = "inputMove()";
+
+		if(mdown) {
+			if(e.touches && e.touches.length > 0) {
+				pos = getMousePos(e.touches[0]);
+			} else {
+				pos = getMousePos(e);
+			}
+			centerPos(pos);
+	//mapCenterPos(pos);
+			toLeftRight(pos);
+			printPos(pos);
+			drawHandle(pos);
+	event.preventDefault();
+		}
+	}
+	function getMousePos(evt) {	
+		var pos = {
+		  x: Math.round(evt.clientX) - Math.round(rect.left),
+		  y: Math.round(evt.clientY) - Math.round(rect.top)
+		};
+		pos.ox = pos.x;
+		pos.oy = pos.y;
+		return pos;
+	}
+	function centerPos(pos) {
+		pos.cx = pos.x-rect.width/2;
+		pos.cy = rect.height/2-pos.y;
+	}
+	function toLeftRight(pos) {
+		pos.left = Math.min(pos.cy+pos.cx, 254);
+		pos.right = Math.min(pos.cy-pos.cx, 254);
+		return pos;
+	}
+	function mapCenterPos(pos) {
+		pos.cx = Math.round(pos.cx.map(0, rect.width/2, 0, 254));
+		pos.cy = 254 - Math.round(pos.cy.map(0, rect.height/2, 0, 254));
+	}
+	function printPos(pos) {
+		document.getElementById("xy").innerHTML = "x:"+pos.x + "  y:" + pos.y;
+		document.getElementById("cxcy").innerHTML = "cx:"+pos.cx + "  cy:" + pos.cy;
+		document.getElementById("lr").innerHTML = "l:"+pos.left + "  r:" + pos.right;
+	}
+	function drawHandle(pos) {
+		ctx.clearRect(0, 0, rect.width, rect.height);
+		var s = 10;
+		ctx.fillRect(pos.ox-s/2, pos.oy-s/2, s, s);
+		
+		ctx.beginPath();
+		ctx.moveTo(rect.width/2, rect.height/2);
+		ctx.lineTo(pos.ox, pos.oy);
+		ctx.stroke();
+	}
+}
